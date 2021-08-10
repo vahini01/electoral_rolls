95434d84abf436bc9141a128af24e7d8a9a298da
Jalend-15 <jalend.bantupalli@gmail.com>
2021-08-10 23:03:49 +0530

    Andhra.py modified

---
+++ andhra/config.py
@@ -0,0 +1,6 @@
+
+
+
+ANDHRA_TRACK_DIR = '/Users/jalend15/PycharmProjects/electoral_rolls/andhra/andhra_pdfs'
+ANDHRA_PDF_ENGLISH_DIR = '/Users/jalend15/PycharmProjects/electoral_rolls/andhra/andhra_pdfs/english'
+ANDHRA_PDF_TELUGU_DIR = '/Users/jalend15/PycharmProjects/electoral_rolls/andhra/andhra_pdfs/telugu'
